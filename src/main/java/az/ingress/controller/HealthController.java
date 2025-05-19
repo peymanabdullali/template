faf4b0a66485137f3e6e1c347d452efa5b6e3dd1
peyman <abdullalipeyman@gmail.com>
2025-05-19 22:47:48 +0400

    try  conflict

--- src/main/java/az/ingress/controller/HealthController.java
+++ src/main/java/az/ingress/controller/HealthController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/health")
-    public ResponseEntity<Void> checkHealthy() {
+    @GetMapping("/healthCheck")
+    public ResponseEntity<String> checkHealthy() {
         return ResponseEntity.ok()
-                             .build();
+                .body("UP");
     }
 }
